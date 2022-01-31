f6654581dae767b3403eda6d870e3118ad1433b8
EC2 Default User <ec2-user@ip-172-31-87-87.ec2.internal>
2022-01-31 19:49:12 +0000

    lab2

---
+++ lec2.py
@@ -0,0 +1,13 @@
+"""
+lec 2
+this is a regional comment
+
+"""
+
+# print("hello world") # this is a single line comment
+#print( type(123))
+#print( type(123.))
+#print( type("123"))
+#print("Tom's cat")
+#print("Hello " + "World")
+print(2**3)
